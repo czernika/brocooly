7894955649f644d2ec8acc7813cfba1076bf20b6
czernika <aliha.devs@gmail.com>
2021-07-17 20:45:04 +0300

    refactor: controller

---
+++ czernika/brocooly/core/Commands.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Console commands loader
+ *
+ * @package Brocooly
+ * @since 0.7.0
+ */
+
+declare(strict_types=1);
+
+namespace Brocooly;
+
+use Brocooly\Console\MakeMenu;
+use Brocooly\Console\MakeShortcode;
+use Brocooly\Console\MakeController;
+
+class Commands
+{
+	/**
+	 * Array of available console commands
+	 *
+	 * @var array
+	 */
+	private static array $commands = [
+		MakeMenu::class,
+		MakeShortcode::class,
+		MakeController::class,
+	];
+
+	/**
+	 * Get all configuration data from `config` folder
+	 */
+	public static function get() {
+		return static::$commands;
+	}
+}
---
+++ czernika/brocooly/core/Console/MakeController.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Create controller
+ *
+ * @example
+ * ```
+ * php broccoli new:controller <ControllerName>
+ * ```
+ *
+ * @package Brocooly
+ * @since 0.7.0
+ */
+
+declare(strict_types=1);
+
+namespace Brocooly\Console;
+
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MakeController extends CreateFileConsoleCommand
+{
+
+	/**
+	 * The name of the command
+	 */
+	protected static $defaultName = 'new:controller';
+
+	/**
+	 * Controller name
+	 *
+	 * @var string
+	 */
+	private string $controller;
+
+	/**
+	 * Shortcode stub file model
+	 *
+	 * @var string
+	 */
+	protected string $stubModelName = 'controller.stub';
+
+	/**
+	 * Set arguments for `configure()` method
+	 */
+	protected function setArguments() {
+		$this
+			->setDescription( 'Allows you to create new controller' )
+			->addArgument(
+				'controller',
+				InputArgument::REQUIRED,
+				'Controller name'
+			);
+	}
+
+	/**
+	 * Set arguments for `execute()` method
+	 */
+	protected function preexecute( InputInterface $input, OutputInterface $output ) {
+		$this->controller = $input->getArgument( 'controller' );
+
+		$this->createFile(
+			$output,
+			$this->controller,
+			'/inc/Controllers/',
+			'Controller.php',
+		);
+
+		return $this->success( $output, 'Controller was successfully created' );
+	}
+
+	/**
+	 * Replace variables inside stub file
+	 *
+	 * @param string $value | value to handle inside.
+	 * @return array
+	 */
+	protected function searchAndReplace( string $value ) {
+		return [
+			'{{ CONTROLLER }}' => Str::ucfirst( $value ),
+		];
+	}
+}
